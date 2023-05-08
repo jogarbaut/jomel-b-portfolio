32845ccb928b0280447fbec507b2f0fa77f7ca4c
Jomel Bautista <jomelgbautista@gmail.com>
2023-05-07 22:02:25 -0700

    updated navbar buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 import useMediaQuery from "../hooks/useMediaQuery"
-import NavLink from './NavLink'
+import NavLink from "./NavLink"
 import { GoThreeBars, GoX } from "react-icons/go"
-import Button from './Button'
+import Button from "./Button"
 
 const Navbar = ({ selectedPage, setSelectedPage }) => {
   const [isUserTopOfPage, setIsUserTopOfPage] = useState(true)
@@ -19,7 +19,7 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
         setIsUserTopOfPage(true)
-        setSelectedPage('home')
+        setSelectedPage("home")
       } else {
         setIsUserTopOfPage(false)
       }
@@ -69,14 +69,14 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
         </NavLink>
       </div>
       <div className="flex items-center justify-between">
-      <a
-            href="/assets/Bautista_Jomel_Resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            download
-          >
-            <Button primary>Download Resume</Button>
-          </a>
+        <a
+          href="/Bautista_Jomel_Resume.pdf"
+          target="_blank"
+          rel="noreferrer"
+          download
+        >
+          <Button primary>Download Resume</Button>
+        </a>
       </div>
     </>
   )
@@ -94,7 +94,7 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
     <div className="fixed right-0 left-0 z-20 min-h-min top-0 w-full bg-white drop-shadow-xl">
       <div className="flex justify-between pt-6 px-4">
         <div className="font-bold">
-          <h1 className='text-xl'>Jomel Bautista</h1>
+          <h1 className="text-xl">Jomel Bautista</h1>
         </div>
         <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
           <GoX className="h-6 w-6 text-gray-400" />
@@ -107,10 +107,10 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
         <NavLink to="techSkills">Tech Skills</NavLink>
         <NavLink to="connect">Connect</NavLink>
         <div className="flex justify-start">
-        <a
-            href="/assets/Bautista_Jomel_Resume.pdf"
+          <a
+            href="/Bautista_Jomel_Resume.pdf"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
             download
           >
             <Button primary>Download Resume</Button>
@@ -122,18 +122,18 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
 
   return (
     <nav className="w-full">
-    <div
-      className={`${navbarBackground} fixed top-0 z-10 flex w-full py-4 transition duration-300`}
-    >
-      <div className="flex flex-row items-center justify-between max-w-7xl w-full mx-auto px-2">
-        <div className="font-semibold">
-          <h1 className='text-xl mr-8'>Jomel Bautista</h1>
+      <div
+        className={`${navbarBackground} fixed top-0 z-10 flex w-full py-4 transition duration-300`}
+      >
+        <div className="flex flex-row items-center justify-between max-w-7xl w-full mx-auto px-2">
+          <div className="font-semibold">
+            <h1 className="text-xl mr-8">Jomel Bautista</h1>
+          </div>
+          {isLargeScreenAndAbove ? renderedFullNavbar : renderedMinimizedNavbar}
         </div>
-        {isLargeScreenAndAbove ? renderedFullNavbar : renderedMinimizedNavbar}
       </div>
-    </div>
-    {!isLargeScreenAndAbove && isMenuToggled && renderedNavbarModal}
-  </nav>
+      {!isLargeScreenAndAbove && isMenuToggled && renderedNavbarModal}
+    </nav>
   )
 }
 
